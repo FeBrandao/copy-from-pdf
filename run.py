d9d04be1cf2c441e7e3e52dea4cc60b4c9b04f66
FeBrandao <87707818+FeBrandao@users.noreply.github.com>
2022-07-07 10:53:33 -0300

    Update run.py

--- run.py
+++ run.py
@@ -8,18 +8,6 @@ def clean_clipboard_text():
     text = pyperclip.paste()
     
     text.replace('\r\n', ' ')
-    
-    text.replace('(A) ', ';')
-    text.replace('(B) ', ';')
-    text.replace('(C) ', ';')
-    text.replace('(D) ', ';')
-    text.replace('(E) ', ';')
-    
-    text.replace('\r\n(A) ', ';')
-    text.replace('\r\n(B) ', ';')
-    text.replace('\r\n(C) ', ';')
-    text.replace('\r\n(D) ', ';')
-    text.replace('\r\n(E) ', ';')
 
     pyperclip.copy(text)
 
