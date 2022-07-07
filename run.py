f71309db36974628ad203b1d9fb87aececdf1abd
FeBrandao <87707818+FeBrandao@users.noreply.github.com>
2022-07-07 09:44:35 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,11 +3,25 @@ import keyboard
 import time
 
 def clean_clipboard_text():
-    time.sleep(0.5)
+    time.sleep(0.8)
     
     text = pyperclip.paste()
+    
+    text.replace('\r\n', ' ')
+    
+    text.replace('(A) ', ';')
+    text.replace('(B) ', ';')
+    text.replace('(C) ', ';')
+    text.replace('(D) ', ';')
+    text.replace('(E) ', ';')
+    
+    text.replace('\r\n(A) ', ';')
+    text.replace('\r\n(B) ', ';')
+    text.replace('\r\n(C) ', ';')
+    text.replace('\r\n(D) ', ';')
+    text.replace('\r\n(E) ', ';')
 
-    pyperclip.copy(text.replace('\r\n', ' '))
+    pyperclip.copy(text)
 
 keyboard.add_hotkey('ctrl+c', clean_clipboard_text)
 
